4f701a234b9580b1aa086b29deedfb33d7d9d412
Mr Fathead <pantokratordios@gmail.com>
2019-03-01 14:32:15 +0100

    First commit

---
+++ JavaApplication6/src/javaapplication6/Igrac.java
@@ -0,0 +1,21 @@
+package javaapplication6;
+
+public class Igrac {
+	public int x=10;
+	public int y=20;
+	Igrac(int x){
+	    this.x=x;	
+		
+	}
+	Igrac(){
+		
+	
+	}
+     Igrac(int x,int y) {
+	
+		this.x=x;
+		this.y=y;
+	}
+	
+
+}
---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,18 @@
+package javaapplication6;
+
+import java.util.Scanner;
+
+public class JavaApplication6 {
+	public static void main(String[] args) {
+		Igrac i1 = new Igrac();
+		System.out.println(i1.x);
+		Igrac i2 = new Igrac(3,4);
+		System.out.println(i2.x);
+		Igrac i3= new Igrac(5);
+		System.out.println(i3.x);
+		
+		
+		
+	}
+
+}
